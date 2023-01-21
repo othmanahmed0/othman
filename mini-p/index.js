138db25f1e684e59c5e40e5b499f77606586be5c
othmanahmed0 <109923821+othmanahmed0@users.noreply.github.com>
2023-01-21 22:04:41 +0300

    Add files via upload

---
+++ mini-p/index.js
@@ -0,0 +1,7 @@
+// const pe =document.querySelectorAll("p")
+// const done = document.getElementById("check-box")
+
+
+// done.addEventListener("click",function(){
+//     document.querySelectorAll("p").style.
+// })
